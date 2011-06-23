9ab15f91f51ba99dc7990747cc36ebdf7f7a77f8
Colin Bell <col@baibell.org>
2011-06-23 11:00:18 +1000

    menu icon

--- rc.lua
+++ rc.lua
@@ -115,7 +115,7 @@ mymainmenu = awful.menu({width = 150,  items = {
   { "Zenix", zenix.menu.Zenix_menu.Zenix, "/usr/share/awesome/themes/zenix/zenix-menu-icon.gif" },
   { "Terminal", "/usr/bin/sakura", "/usr/share/icons/hicolor/48x48/apps/xfce-terminal.png" },
   { "File Manager", "/usr/bin/pcmanfm", "/usr/share/icons/gnome-dust/32x32/apps/redhat-filemanager.png" },
-  { "awesome", myawesomemenu, "/usr/share/awesome/themes/zenix/awesome-icon.png" },
+  { "awesome", myawesomemenu, beautiful.awesome_icon },
   { "Logout", awesome.quit,"/usr/local/share/icons/bullet_black.png" },
   { "Shutdown", "sudo shutdown -h now","/usr/local/share/icons/shutdown.png" },
   { "Reboot", "sudo reboot","/usr/local/share/icons/reboot.png" },
